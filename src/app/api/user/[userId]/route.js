077224d038358747b62fa1c22db22fa7bb34faed
Hitesh Bhoot <hiteshbhoot938@gmail.com>
2024-08-31 21:39:36 +0530

    "Added database connection and CRUD operations for user API route"

--- src/app/api/user/[userId]/route.js
+++ src/app/api/user/[userId]/route.js
@@ -1,15 +1,51 @@
 import { NextResponse } from "next/server";
+import { connectDB } from "@/helpers/db/db";
+import User from "@/Model/user/userModel";
+connectDB();
 
-export const GET = (request, { params }) => {
+export const GET = async (request, { params }) => {
   console.log(params);
 
+  const { userId } = params;
+
+  const user = await User.findById(userId);
+  if (!user) {
+    return NextResponse.json(
+      { status: "error", message: "User not found" },
+      { status: 404 }
+    );
+  }
+  return NextResponse.json({ status: "success", user }, { status: 200 });
+};
+
+export const PATCH = async (request, { params }) => {
+  console.log(params);
+  const { userId } = params;
+
+  const body = await request.json();
+
+  const user = await User.findByIdAndUpdate(userId, body, { new: true });
+  if (!user) {
+    return NextResponse.json(
+      { status: "error", message: "User not found" },
+      { status: 404 }
+    );
+  }
+  return NextResponse.json({ status: "success", user }, { status: 200 });
+};
+
+export const DELETE = async (request, { params }) => {
+  console.log(params);
+  const { userId } = params;
+  const user = await User.findByIdAndDelete(userId);
+  if (!user) {
+    return NextResponse.json(
+      { status: "error", message: "User not found" },
+      { status: 404 }
+    );
+  }
   return NextResponse.json(
-    {
-      message: "Hello Nextjs",
-      status: "success",
-    },
-    {
-      status: 200,
-    }
+    { status: "success", message: "User deleted" },
+    { status: 200 }
   );
 };
