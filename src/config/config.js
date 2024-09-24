062e35189926569ffba08a122092ba8cd2a79619
Hitesh Bhoot <hiteshbhoot938@gmail.com>
2024-09-24 21:02:19 +0530

    "Swap BASE_URL values in config.js"

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  BASE_URL: "http://localhost:3000",
-  // BASE_URL: "https://work-manager-by-hhb.vercel.app",
+  // BASE_URL: "http://localhost:3000",
+  BASE_URL: "https://work-manager-by-hhb.vercel.app",
 };
